c2feef87ff241cc43730e450e4eba530548a9ff5
erdeks <aponcelasgarcia@iesesteveterradas.cat>
2017-10-23 17:46:20 +0200

    fichero almost done

--- ranking.php
+++ ranking.php
@@ -5,6 +5,19 @@
   <title>Ranquing</title>
 </head>
 <body>
+  <?php
+    $nombre = "puntaciones.txt"
+    $fichero = fopen($nombre, "r");
+
+    $contenido = array();
+    while (!feof($fichero)){
+      $linea = fgets($file);
+      $pos = strpos($linea, ",");
+      $jugador = substr($linea, 0, $pos);
+      $score = substr($linea, 0, $pos+1);
+    }
+
+   ?>
   <h4>Quieres Jugar Otra Vez? </h4>
   <form action="juego.php" method="post">
     <input name="nombre" placeholder="Nombre: "></br></br>
